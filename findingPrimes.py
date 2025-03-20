15f6a8725ce86f59cbaf9e5f5428b885c788fd73
AnkurNahar <ankurnahar.an@gmail.com>
2025-03-19 19:30:32 -0700

    findingPrimes

---
+++ findingPrimes.py
@@ -0,0 +1,11 @@
+def allPrimesUpTo(num):
+    primes = [2]
+    for number in range(2, num):
+        sqrt = number ** 0.5
+        for factor in primes:
+            if number % factor == 0:
+                break
+            if factor > sqrt:
+                primes.append(number)
+                break
+    return primes
